f3039ecf960b90b945a0d360ebb516cf27f9acd7
Ekdanai Deeboonchai <ekdanai.d@gmail.com>
2019-07-23 14:21:29 +0700

    Render Song as a List

---
+++ client/component/SongList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+  renderSong() {
+    return this.props.data.songs.map(song => {
+      return <li key={song.id}>{song.title}</li>;
+    });
+  }
+  render() {
+    console.log(this.props);
+    if (this.props.data.loading) {
+      return <div>Loading ...</div>;
+    }
+    return <div>{this.renderSong()}</div>;
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+export default graphql(query)(SongList);
